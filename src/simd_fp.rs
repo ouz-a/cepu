36da7cc6915ba821a93059760e72ace92449b296
Oğuz Ağcayazı <ouz.agz@gmail.com>
2026-01-19 21:57:04 +0300

    add cmeqreg

--- src/simd_fp.rs
+++ src/simd_fp.rs
@@ -810,7 +810,7 @@ pub struct CmeqVector {
 
 impl CmeqVector {
     pub fn exec(self, cpu: &mut Cpu, _old_pc: u64) {
-        if ((self.size << 2) | self.q) == 0b110 {
+        if ((self.size << 1) | self.q) == 0b110 {
             panic!("Undefined CMEQ");
         }
 
@@ -863,4 +863,99 @@ impl CmeqScalar {
         value: 0b0101_1110_1110_0000_1001_1000_0000_0000,
         decode: Self::decode,
     };
+}
+
+#[derive(Debug, Clone, Copy)]
+/// Compare bitwise equal (vector)
+pub struct CmeqRegVector {
+    pub q: u8,
+    pub size: u8,
+    pub rm: u8,
+    pub rn: u8,
+    pub rd: u8,
+}
+
+impl CmeqRegVector {
+    pub fn exec(self, cpu: &mut Cpu, _old_pc: u64) {
+        if ((self.size << 1) | self.q) == 0b110 {
+            panic!("Undefined CMEQ");
+        }
+
+        let esize = 8 << self.size;
+        let datasize = 64 << self.q;
+        let elements = datasize / esize;
+
+        instruction_cmeq_req(cpu, self.rn, self.rm, self.rd, esize, datasize, elements);
+    }
+
+    pub fn decode(word: u32) -> Instruction {
+        let q = get_bits_ct!(word, 30, 1) as u8;
+        let size = get_bits_ct!(word, 22, 2) as u8;
+        let rm = get_bits_ct!(word, 16, 5) as u8;
+        let rn = get_bits_ct!(word, 5, 5) as u8;
+        let rd = get_bits_ct!(word, 0, 5) as u8;
+        Instruction::CmeqRegVector(Self { q, size, rm, rn, rd })
+    }
+
+    pub const CMEQ_REG_VECTOR: InstDesc = InstDesc {
+        mask: 0b1011_1111_0010_0000_1111_1100_0000_0000,
+        value: 0b0010_1110_0010_0000_1000_1100_0000_0000,
+        decode: Self::decode,
+    };
+}
+
+#[derive(Debug, Clone, Copy)]
+/// Compare bitwise equal (vector)
+pub struct CmeqRegScalar {
+    pub rm: u8,
+    pub rn: u8,
+    pub rd: u8,
+}
+
+impl CmeqRegScalar {
+    pub fn exec(self, cpu: &mut Cpu, _old_pc: u64) {
+        let esize = 8 << 0b11;
+        let datasize = esize;
+        let elements = 1;
+
+        instruction_cmeq_req(cpu, self.rn, self.rm, self.rd, esize, datasize, elements);
+    }
+
+    pub fn decode(word: u32) -> Instruction {
+        let rm = get_bits_ct!(word, 16, 5) as u8;
+        let rn = get_bits_ct!(word, 5, 5) as u8;
+        let rd = get_bits_ct!(word, 0, 5) as u8;
+        Instruction::CmeqRegScalar(Self { rm, rn, rd })
+    }
+
+    pub const CMEQ_REG_SCALAR: InstDesc = InstDesc {
+        mask: 0b1111_1111_1110_0000_1111_1100_0000_0000,
+        value: 0b0111_1110_1110_0000_1000_1100_0000_0000,
+        decode: Self::decode,
+    };
+}
+
+pub fn instruction_cmeq_req(
+    cpu: &mut Cpu,
+    rn: u8,
+    rm: u8,
+    rd: u8,
+    esize: u8,
+    datasize: u8,
+    elements: u8,
+) {
+    let mut result = 0;
+    let operand1 = cpu.v_read(rn.into(), datasize);
+    let operand2 = cpu.v_read(rm.into(), datasize);
+
+    for e in 0..elements {
+        let element1 = operand1.bits_get(e, esize);
+        let element2 = operand2.bits_get(e, esize);
+        if element1 == element2 {
+            result = result.bits_set(e, esize, 0b1.replicate(esize));
+        } else {
+            result = result.bits_set(e, esize, 0b0.replicate(esize));
+        };
+    }
+    cpu.v_write(rd.into(), datasize, result);
 }
